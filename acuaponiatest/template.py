e253a57ba1eb2bef7e74ea27983494f0e9672d1b
Daniel <dreyeslastiri@users.noreply.github.com>
2018-01-19 01:01:45 +0100

    Add files via upload

---
+++ acuaponiatest/template.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: daniel
+"""
+from acuaponiatest.acuaponiatest.folder1.printstring import testing
+
+string = 'This is a test string'
+long = testing(string)
